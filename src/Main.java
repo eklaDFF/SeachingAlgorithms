b8b81238e0e83669dc012765a5cec1beb9032ead
eklaDFF <rahulkrsingh160086@gmail.com>
2024-01-23 14:33:39 +0530

    Added UnOrdered Linear Search Algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-------------------------Example for UnOrderedLinearSearch");
+        int[] unOrdArr = {22,43,12,-9,0,32};
+        int index = UnOrderedLinearSearch(unOrdArr,12);
+        System.out.println("Element found at index " + index);
+        index = UnOrderedLinearSearch(unOrdArr,8);
+        System.out.println("Element found at index " + index);
+    }
+
+    public static int UnOrderedLinearSearch(int[] unSortedArray,int number){
+        for (int i=0;i<unSortedArray.length;i++){
+            if (unSortedArray[i]==number){
+                return i;
+            }
+        }
+        return -1;
     }
 }
