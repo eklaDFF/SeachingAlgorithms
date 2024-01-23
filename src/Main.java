a9392d4b3897a77322d82d2670df81b09bf8646c
eklaDFF <rahulkrsingh160086@gmail.com>
2024-01-23 14:48:56 +0530

    Ordered Linear Search Algorithm Added

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,32 @@ public class Main {
         System.out.println("Element found at index " + index);
         index = UnOrderedLinearSearch(unOrdArr,8);
         System.out.println("Element found at index " + index);
+
+
+        System.out.println("-------------------------Example2 for OrderedLinearSearch");
+        int[] ordArr = {8,9,43,47,95,100,302};
+        int index2 = OrderedLinearSearch(ordArr,100);
+        System.out.println("Element found at index " + index2);
+        index2 = OrderedLinearSearch(ordArr,22);
+        System.out.println("Element found at index " + index2);
     }
 
     public static int UnOrderedLinearSearch(int[] unSortedArray,int number){
         for (int i=0;i<unSortedArray.length;i++){
             if (unSortedArray[i]==number){
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int OrderedLinearSearch(int[] sortedArray,int number){
+        for (int i=0;i<sortedArray.length;i++){
+            if (sortedArray[i]==number){
+                return i;
+            }
+            if (sortedArray[i]>number){
+                break;
             }
         }
         return -1;
